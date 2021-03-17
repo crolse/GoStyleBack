8298472dc92e06696f4743d960d25f83803732e0
GregoireEpsi <gregoire.barre@epsi.fr>
2021-03-17 16:51:53 +0100

    feat : hash password

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,63 +1,67 @@
 const db = require("../models");
 const User = db.user;
+var passwordHash = require('password-hash');
 const Op = db.Sequelize.Op;
 
 // Create and Save a new User
 exports.create = (req, res) => {
-   // Validate request
-   if (!req.body.mail && !req.body.password) {
-    res.status(400).send({
-      message: "mail et/ou mot de passe vide"
-    });
-    return;
-  }
+    // Validate request
+    if (!req.body.mail && !req.body.password) {
+        res.status(400).send({
+            message: "mail et/ou mot de passe vide"
+        });
 
-  // Create a User
-  const user = {
-    mail: req.body.mail,
-    password: req.body.password,
-    
-  };
+        return;
+    }
 
-  // Save User in the database
-  User.create(user)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Une erreur c'est produit lors de la création de l'utilisateur"
-      });
-    });
+
+
+    // Create a User
+    const user = {
+        mail: req.body.mail,
+        password: passwordHash.generate(req.body.password)
+
+    };
+
+    // Save User in the database
+    User.create(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Une erreur c'est produit lors de la création de l'utilisateur"
+            });
+        });
 };
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  
+
 };
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
-  
+
 };
 
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
-  
+
 };
 
 // Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
-  
+
 };
 
 // Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-  
+
 };
 
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
-  
+
 };
