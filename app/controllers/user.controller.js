c02004625a5265c16f5e993214f0398e5f127770
GregoireEpsi <gregoire.barre@epsi.fr>
2021-03-17 17:38:17 +0100

    feat :sign up user ok

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,7 +3,7 @@ const User = db.user;
 var passwordHash = require('password-hash');
 const Op = db.Sequelize.Op;
 
-// Create and Save a new User
+//#region  Create a new User
 exports.create = (req, res) => {
     // Validate request
     if (!req.body.mail && !req.body.password) {
@@ -14,7 +14,7 @@ exports.create = (req, res) => {
         return;
     }
 
-
+    //duplicate mail verification
     User.count({ where: { mail: req.body.mail } }).then((value) => {
         console.log(value);
         if (value != 0) {
@@ -54,33 +54,5 @@ exports.create = (req, res) => {
 
 
 };
+//#endregion
 
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {
-
-};
-
-// Find a single Tutorial with an id
-exports.findOne = (req, res) => {
-
-};
-
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {
-
-};
-
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-
-};
-
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-
-};
-
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-
-};
